908a4d4378d16437d4908e3725266b90711b092e
rixo <rixo@rixo.fr>
2019-10-17 00:57:43 +0200

    remove cjs config: nollup now supports ES config natively

--- .nolluprc.js
+++ .nolluprc.js
@@ -1,9 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  // NOTE Nollup does not support ES6 imports in config. You can use the `esm`
-  //      package if your rollup config is ES6.
-  config: path.resolve(__dirname, 'rollup.config.cjs'),
   hot: true,
   contentBase: './public',
   port: 5000,
