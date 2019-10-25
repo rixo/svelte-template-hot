6be28f26a67755218e2061e7f54fbb035c874865
rixo <rixo@rixo.fr>
2019-10-25 04:44:11 +0200

    clean mess in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,11 @@
-<script context="module">
-// debugger
-</script>
-
 <script>
 	export let name;
-	// throw new Error('what??')
 </script>
 
 <style>
 	h1 {
-		color: lightgreen;
-	}
-	h2 {
-		font-size: small;
-		color: red;
-	}
-	h3 {
-		color: gray;
+		color: purple;
 	}
 </style>
 
-<h1>Hello {name}</h1>
-
-<h2>Howdy?</h2>
-
-<h3>G R E A T</h3>
+<h1>Hello {name}!</h1>
