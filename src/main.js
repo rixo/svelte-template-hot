55a11686984821e2a573f3f7c93f5ad454c14337
rixo <rixo@rixo.fr>
2019-11-25 13:29:02 +0100

    add root accept handler

--- src/main.js
+++ src/main.js
@@ -8,3 +8,11 @@ const app = new App({
 });
 
 export default app;
+
+// recreate the whole app if an HMR update touches this module
+if (import.meta.hot) {
+	import.meta.hot.dispose(() => {
+		app.$destroy()
+	})
+	import.meta.hot.accept()
+}
