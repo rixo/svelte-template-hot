fade275b5c5d397e249157b4a6376816496eb44d
frederikhors <41120635+frederikhors@users.noreply.github.com>
2020-10-15 19:01:44 +0200

    merge from upstream official template

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-import App from './App.svelte'
+import App from './App.svelte';
 
 const app = new App({
-  target: document.body,
-  props: {
-    name: 'world',
-  },
-})
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
 
-export default app
+export default app;
+
 
 // recreate the whole app if an HMR update touches this module
 if (import.meta.hot) {
-  import.meta.hot.dispose(() => {
-    app.$destroy()
-  })
-  import.meta.hot.accept()
+	import.meta.hot.dispose(() => {
+		app.$destroy()
+	})
+	import.meta.hot.accept()
 }
